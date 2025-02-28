713ab62abe3afc48c543cc30bea571d262bd602f
Carson Smith <csm2356@wgu.edu>
2025-02-27 20:21:22 -0600

    added event handler to get value of the button clicked.

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-let calculation;
+const buttons = document.querySelectorAll('.button');
+let buttonValue;
+let calculation = parseInt(document.querySelector(".screen").value);
+
+buttons.forEach(button => {
+    button.addEventListener('click', clickHandler)
+});
+
+function clickHandler(event) {
+    const button = event.target;
+    buttonValue = button.value;
+    if (!isNaN(buttonValue)) {
+        buttonValue = parseInt(buttonValue);
+    }
+    console.log(buttonValue);
+}
 
 function updateAnswer(value) {
     calculation += value;
     document.querySelector(".screen").innerText = calculation;
     console.log(calculation);
 }
-
